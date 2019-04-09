230c96e645c61ce2fea18dfe7d662c1ca9af3af5
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-09 11:40:40 -0700

    Add test proxy target at a specific restaurant url

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
+var proxy = require('http-proxy-middleware');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', proxy({ target: 'http://localhost:3001/r/eoss-oharahaven/', changeOrigin: true }))
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
